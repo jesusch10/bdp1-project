b4c518d100a16606fa7e10b3ebad682dd7c3e0bc
jesusch10 <136498796+jesusch10@users.noreply.github.com>
2023-06-29 13:05:13 +0200

    Add files via upload

---
+++ align.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import sys,os
+from timeit import default_timer as timer
+
+# Control variables
+##############################################
+
+#bwa aln -t 1 /hg19/hg19bwaidx myread.fa > myread.sai
+#bwa samse -n 10  /hg19/hg19bwaidx myread.sai myread.fa > myread.sam
+
+
+dbpath = "/data/phd-2018/hg19/"
+dbname = "hg19bwaidx"
+
+queryname = "read200.fa"
+
+out_name = "read200"
+
+md5file = "md5.txt"
+
+
+command = "./bwa aln -t 1 " + dbpath + dbname + " " + queryname + " > " + out_name + ".sai"
+print "launching command: " , command
+os.system(command)
+
+command = "./bwa samse -n 10 " + dbpath + dbname + " " + out_name + ".sai " + queryname + " > " + out_name + ".sam"
+print "launching command: " , command
+os.system(command)
+
+print "Creating md5sums"
+os.system("md5sum " + out_name + ".sai " + " > " + md5file)
+os.system("md5sum " + out_name + ".sam " + " >> " + md5file)
+
+print "gzipping out text file"
+command = "gzip " + out_name + ".sam"
+print "launching command: " , command
+os.system(command)
+
+print "exiting"
+
+exit(0)
