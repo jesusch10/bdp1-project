5c54f42fe4b674d30bd793aae675254ddb79b42e
jesusch10 <136498796+jesusch10@users.noreply.github.com>
2023-07-05 14:12:30 +0200

    Create align.py

---
+++ align.py
@@ -0,0 +1,29 @@
+#!/ usr/ bin / python
+import sys, os
+from timeit import default_timer as timer
+
+# Control variables
+start = timer()
+dbpath = "/data2/BDP1_2022/hg19/"
+dbname = "hg19bwaidx"
+queryname = sys.argv[1]
+out_name = queryname[: -3]
+md5file = "md5.txt"
+
+command = "/data2/BDP1_2022/condor/hg/bwa aln -t 1 " + dbpath + dbname + " " + queryname + " > " + out_name + ".sai"
+print "Launching command: " , command
+os.system (command)
+command = "/data2/BDP1_2022/condor/hg/bwa samse -n 10 " + dbpath + dbname + " " + out_name + ".sai " + queryname + " > " + out_name + ".sam"
+print "Launching command: " , command
+os.system (command)
+print "Creating md5sums"
+os.system ("md5sum " + out_name + ".sai " + " > " + md5file)
+os.system ("md5sum " + out_name + ".sam " + " >> " + md5file)
+print "gzipping out text file "
+command = " gzip " + out_name + ".sam "
+print "Launching command: " , command
+os.system(command)
+execution_time = timer() - start
+print "Total execution time: " + str(execution_time)
+print "Exiting "
+exit(0)
